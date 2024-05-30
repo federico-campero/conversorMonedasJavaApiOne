bd1d69dc995e1acd1f7ee69af5d5429946840b31
federico-campero <federicoecampero@gmail.com>
2024-05-30 00:56:48 -0300

    implemantacion de todas las opciones para el usuario

--- src/Principal.java
+++ src/Principal.java
@@ -15,9 +15,9 @@ public class Principal {
 
         String bienvenida = """
                 ----------------------------------------------------
-                
+                                
                 Bienvenido/a al Conversor de Monedas
-                
+                                
                 1) Dolar a Peso Argentino
                 2) Peso Argentino a Dolar
                 3) Dolar a Peso Chileno
@@ -25,39 +25,69 @@ public class Principal {
                 5) Dolar a Real Brasileño
                 6) Real Brasileño a Dolar
                 7) salir
-                
+                                
                 Indique que conversion desea realizar:
-      
+                      
                 ----------------------------------------------------
                 """;
 
         System.out.println(bienvenida);
 
 
-        while(true){
+        while (true) {
 
-            int eleccion =  lectura.nextInt();
+            int eleccion = lectura.nextInt();
 
-            if(eleccion == 7){
+            if (eleccion == 7) {
                 break;
             }
-            if(eleccion == 1){
 
-                System.out.println("Ingrese el importe que desea convertir");
-                double importe = lectura.nextDouble();
+            System.out.println("Ingrese el importe que desea convertir");
+            double importe = lectura.nextDouble();
+
+            if (eleccion == 1) {
 
                 double conversionRealizada = calculadoraConversion.deDolarAPesoArgentino(importe);
-
                 System.out.println(conversionRealizada);
+                continue;
+            }
 
+            if (eleccion == 2) {
+
+                double conversionRealizada = calculadoraConversion.dePesoArgentinoADolar(importe);
+                System.out.println(conversionRealizada);
+                continue;
+            }
+            if (eleccion == 3) {
+
+                double conversionRealizada = calculadoraConversion.deDolarAPesoChileno(importe);
+                System.out.println(conversionRealizada);
                 continue;
             }
 
 
+            if (eleccion == 4) {
+
+                double conversionRealizada = calculadoraConversion.dePesoChilenoADolar(importe);
+                System.out.println(conversionRealizada);
+                continue;
+            }
+
+            if (eleccion == 5) {
+
+                double conversionRealizada = calculadoraConversion.deDolarARealBRasilenio(importe);
+                System.out.println(conversionRealizada);
+                continue;
+            }
+            if (eleccion == 6) {
+
+                double conversionRealizada = calculadoraConversion.deRealBRasilenioADolar(importe);
+                System.out.println(conversionRealizada);
+                continue;
+            }
 
 
         }
-
 
 
     }
